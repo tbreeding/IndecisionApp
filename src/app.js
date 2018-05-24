d4678df5ed23bc82b4796e43c52dff9127a012c3
Timothy A Breeding <tim.breeding@yahoo.com>
2018-05-24 09:55:09 +0200

    Upload Project

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import React, {Component} from "react";
+import ReactDOM from 'react-dom';
+import IndecisionApp from "./components/IndecisionApp";
+import "normalize.css/normalize.css";
+import "./styles/styles.scss";
+
+
+ReactDOM.render(<IndecisionApp />, document.getElementById("app"));
